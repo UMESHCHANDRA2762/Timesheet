63abfe1e784c9407e6c08c61a3a357451d093b3b
Umesh Chandra <umesh@pranathiss.local>
2025-06-16 01:36:08 -0400

    many changes have been made

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useLocation,
+} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./utils/leafletIconFix";
+
+import Booking from "./pages/Booking";
+import RideRequest from "./pages/RideRequest";
+import Tracking from "./pages/Tracking";
+
+const AppNavbar = () => {
+  const location = useLocation();
+  if (location.pathname.startsWith("/tracking")) return null;
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          Booking
+        </Link>
+        <div className="navbar-nav">
+          <Link className="nav-link" to="/">
+            Book a Ride
+          </Link>
+          <Link className="nav-link" to="/request/RIDE-XYZ">
+            passenger
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <AppNavbar />
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Booking />} />
+          <Route path="/request/:rideId" element={<RideRequest />} />
+          <Route path="/tracking/:rideId" element={<Tracking />} />
+          <Route path="/tracking" element={<Tracking />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
